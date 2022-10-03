345718179e82e1a9b0361515006badabc3280d35
gajendiranlithi <107409557+gajendiranlithi@users.noreply.github.com>
2022-10-03 19:10:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="height">
+            <div class="name1">FLOMIC<br/>
+                Logistic PVT.LTD
+            </div>
+            <div class="name2">
+                <ul>
+                    <li style={{backgroundColor:"rgb(24, 181, 24)",padding: "5px 5px"}}>Home</li>
+                    <li>pages</li>
+                    <li>Dropdown</li>
+                    <li>Link text</li>
+                    <li>Link text</li>
+                </ul>
+            </div>
+        </div>
+        <div class="divname">
+            <div class="div1" style={{margin: "0px 15px" ,backgroundColor: "rgb(140, 203, 140)",textAlign: "center"}}><br/>
+                 QUISQUE VEHICULA URNA AMET
+                <p style={{textAlign:"center",backgroundColor: "azure"}}>colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/>
+                colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/></p>
+                <p style={{fontSize:"larger"}}>Read more</p>
+            </div>
+            <div class="div2" style={{margin: "0px 15px", backgroundColor: "rgb(140, 203, 140)",textAlign: "center"}}><br/>
+                QUISQUE VEHICULA URNA AMET
+                <p style={{textAlign:"center",backgroundColor: "azure"}}>colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/>
+                colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/></p>
+                <p style={{fontSize:"larger"}}>Read more</p>
+            </div>
+            <div class="div3" style={{margin: "0px 15px", backgroundColor: "rgb(140, 203, 140)",textAlign: "center"}}><br/>
+                QUISQUE VEHICULA URNA AMET
+                 <p style={{textAlign:"center",backgroundColor: "azure"}}>colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/>
+                colour, also spelled color<br/><br/>the aspect of any object<br/><br/> that may be described in terms of hue<br/><br/></p>
+                <p style={{fontSize:"larger"}}>Read more</p>
+            </div>
+        </div>
+        <div class="hi1">
+            <div class="hi2">
+                <div style={{padding:"5px 0px" , fontSize: "larger"}}>
+                    MAECENAS LIBERO NUNC EU ERAT
+                </div><br/>
+                <div class="class1"> 
+                    
+                </div>
+            </div>
+            <div class="class2">
+                <div style={{padding:"5px 0px", fontSize: "larger"}}>
+                    MAECENAS LIBERO NUNC EU ERAT
+                </div><br/>
+                <div>
+                    <img src="images/download.jpg" style={{float: "left",width: "100px", border: "1px solid black", padding: "5px",marginRight: "20px"}} alt=""/>
+                    <p><span style={{fontSize:"20px"}} >Heading</span><br/>by<span style={{color: "green"}}> name</span> Friday, 6th April 2045 section</p>
+                    <p>  A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic<br/></p>
+                    <button style={{border:"1px solid rgb(24, 181, 24)",padding: "5px"}}>Read More</button> 
+                </div>
+                <hr/><br/>
+                <div>
+                    <img src="images/download.jpg" style={{float:"left",width:"100px",border:"1px solid black",padding:"5px",marginRight: "20px" }}alt=""/>
+                    <p><span style={{fontSize:"20px"}}  >Heading</span><br/>by<span style={{color: "green"}}> name</span> Friday, 6th April 2045 section</p>
+                    <p>  A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic<br/></p>
+                    <button style={{border:"1px solid rgb(24, 181, 24)"}}>Read More</button>                    
+                </div>
+                <hr/><br/>
+                <div>
+                    <img src="images/download.jpg"  style={{float:"left",width:"100px",border:"1px solid black",padding:"5px",marginRight: "20px" }}alt=""/>
+                    <p><span style={{fontSize:"20px "}} >Heading</span><br/>by<span style={{color: "green"}}> name</span> Friday, 6th April 2045 section</p>
+                      <p>  A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic<br/></p>
+                    <button style={{border:"1px solid rgb(24, 181, 24)", padding: "5px"}}>Read More</button>                    
+                </div>
+            </div>
+        </div>
+        <div style={{textAlign:"center", padding: "2% 15%"}}>
+            <span style={{fontSize:"larger"}}>DONEC FACILISIS VOLUTPAT LIGULA NEC EGESTAS</span><br/>
+            Interger various enim ID augue facibus mattis.pellentesqe nibh eu mi convallis, ac feugiat nulla tincidunt 
+        </div>
+        <div class="style">
+            <div class="float1">   
+                <div class="float11"><img src="images/cssimage.jpg" width="250px" alt=""/></div>
+                <div class="float12" style={{backgroundColor: "orange", fontSize: "larger", textAlign: "left", padding: "5px", height:"167px", marginLeft: "-16px"}}>QUISQUE VEHICULA<br/> URNA SIT AMET<br/> PULVINAR DAIBUS<br/>
+                <button style={{border: "1px solid"}}>Read more</button>
+                </div>
+            </div>
+            <div class="float2">   
+                <div class="float21"><img src="images/cssimage.jpg" width="250px" alt=""/></div>
+                <div class="float22" style={{backgroundColor: "aqua", fontSize: "larger", textAlign: "left", padding: "5px" ,height: "167px",marginLeft: "-16px"}}>QUISQUE VEHICULA<br/> URNA SIT AMET<br/> PULVINAR DAIBUS<br/>
+                <button style={{border: "1px solid"}}>Read more</button>
+                </div>
+            </div>
+        </div>    
+        <div class="span">
+            <div class="span1">
+                <span style={{fontSize:"larger"}}>LOROM IPSUM DOLOR</span><hr/>
+                <p>Donec sed ultrices eros.Nulla fucilisi.Aliquam purus urna,porta non facibus nec,luctus vitae ante.Susspendisses potenti.Morbi Sollicitusin viverra urna,vel anctor purus volupat vel.</p><br/><br/>
+                <img src="images/location.jpg" width="20px" alt=""/> Street name & Number,Town,postcode/Zip<br/>
+                <img src="images/phone.png" width="20px" alt=""/> +00 (123) 456 7890<br/>
+                <img src="images/message.png" width="20px" alt=""/> info@domain.com
+            </div>
+            <div class="span2">
+                <div style={{width: "50%", float: "left"}}>
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>QUICK LINKS</span><hr/>
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>Home page</span><br/><br/>
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>Blog</span><br/><br/>
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>Gallery</span><br/><br/>
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>Portfolio</span><br/><br/>   
+                    <span style={{fontSize:"larger",padding: "5px 0px"}}>Portfolio</span>                
+                </div>
+                <div style={{width:"50%", float: "right"}}>
+                    <span style={{fontSize:"larger"}}>KEEP IN TOUCH</span><hr/>
+                    <input type="text" placeholder="Email" style={{width: "235px", height: "30px", color: "azure", backgroundColor: "black"}}/><br/><br/>
+                    <input type="SUBMIT" style={{backgroundColor:"rgb(101, 165, 101)", fontSize: "larger", color: "azure"}}/><br/><br/><br/><br/>
+                    <img src="images/images.png" width="235px" height="40px" alt=""/> 
+                </div>
+            </div>
+        </div>   
+        <div style={{width: "100%", backgroundColor:"black",color:"azure",padding:"2% 15%", fontSize:"larger"}}>
+            Copyright @ 2015 - All Rights Reserved - Flomic
+        </div>    
+    </div>
+  );
+}
+
+export default App;
